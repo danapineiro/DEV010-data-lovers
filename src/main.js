22fb0eae746072f1185f104cc7c592886d59845e
Regina <regina.esluka@gmail.com>
2023-08-14 09:39:42 -0600

    Filtrado de cartas con menu de opciones

--- src/main.js
+++ src/main.js
@@ -2,47 +2,68 @@ import tarot from './data/tarot/tarot.json' assert {type: 'json'};
 
 const principalDiv = document.createElement("div");
 
-for (let i = 0; i < tarot.cards.length; i ++){
+// variable que tomaran los valores de los selects
+let arcano;
+let tipo;
 
-  const containerCarta = document.createElement("div");
+// funcionalidad cuando el selector de arcanos cambie
+const selectArcanos = document.getElementById("selectArcanos");
+selectArcanos.addEventListener("change", (e) => { // e= es el option al que elegi en el menu de opciones.
+  arcano = e.target.value;// se le agrega el e.target.value, donde value trae el valor de option que es minor o major.
+  const arcanoSelected = tarot.cards.filter((obj) => obj.type === arcano);
+  showCards(arcanoSelected);
+});
 
-  const containerButtons = document.createElement("div");
+// funcionalidad cuando el selector de tipos cambie
+const selectTipos = document.getElementById("selectTipos");
+selectTipos.addEventListener("change", (e) => {
+  tipo = e.target.value
+  const tiposSelected = tarot.cards.filter((obj) => obj.suit === tipo);
+  showCards(tiposSelected);
+});
 
-  const img = document.createElement("img");
-  
-  const buttonLeft = document.createElement("button");
+// Aqui se manda llamar todas las cartas con el evento listener cuando se hace click. Se llama showcards y se le pasa todo lo que trae
+//el array de cards del objeto json. 
+const btnAllCards = document.getElementById("btnAllCards");
+btnAllCards.addEventListener("click", () => {
+  showCards(tarot.cards); //tarot es el objeto declarado en el archivo tarot.json y cards es la llave que nos regresa el array.
+})
 
-  buttonLeft.innerText = "Izquierdo";
+function showCards(cards) { //showcards recibe un array de cartas 
+  removeChildren(principalDiv);
+  // for que muestra las cartas del tarot en la pantalla
+  for (let i = 0; i < cards.length; i++) {
+    const containerCarta = document.createElement("div");
+    const containerButtons = document.createElement("div");
+    const img = document.createElement("img");
 
-  const buttonRight = document.createElement("button");
+    const buttonLeft = document.createElement("button");
+    buttonLeft.innerText = "Izquierdo";
+    const buttonRight = document.createElement("button");
+    buttonRight.innerText = "Derecho";
+    
+    img.classList.add("imgClass");
 
-  img.style.borderRadius = "15px";
+    img.src = cards[i].img;
 
-  containerCarta.style.margin = "25px";
+    containerCarta.classList.add("card");
+    containerCarta.setAttribute("name", cards[i].name)
 
-  buttonRight.innerText = "Derecho";
+    containerButtons.appendChild(buttonLeft);
+    containerButtons.appendChild(buttonRight);
 
-  containerButtons.style.display = "flex";
+    containerCarta.appendChild(img);
+    containerCarta.appendChild(containerButtons);
+    principalDiv.id = "principalDiv";
+    principalDiv.appendChild(containerCarta);
 
-  containerButtons.style.justifyContent = "space-between"
-
-  img.src = tarot.cards[i].img;
-
-  containerCarta.style.display = "inline-block";
-
-  img.style.maxHeight = "250px";
-
-  img.style.width = "auto";
-
-  containerCarta.classList.add("card");
-
-  containerButtons.appendChild(buttonLeft);
-  containerButtons.appendChild(buttonRight);
-
-  containerCarta.appendChild(img);
-  containerCarta.appendChild(containerButtons);
-
-  principalDiv.appendChild(containerCarta);
+  }
+  // append que manda todas las cartas al html
+  document.getElementById("cards").appendChild(principalDiv);
 }
-document.getElementById("cards").appendChild(principalDiv);
 
+function removeChildren(component){
+  while(component.firstChild){
+    component.removeChild(component.firstChild);
+}
+}
